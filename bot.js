11c6219191f95278444f3ff8117f6db234a5e534
Brian Engelhardt <briatt22@gmail.com>
2017-07-19 00:08:40 -0500

    Another fun phrase

--- bot.js
+++ bot.js
@@ -11,7 +11,8 @@ var phraseArray = [
 	'This is a major test of the emergency Aywas broadcasting system',
 	'This is not a real Aywas emergency',
 	'Aywas bot 4 life!',
-	'Signs you might be addicted to aywas: You made a bot for scav'
+	'Signs you might be addicted to aywas: You made a bot for scav',
+	'Tired of cats? Try a Melo!'
 ];
 function chooseRandom(myArray) {
 	return myArray[Math.floor(Math.random() * myArray.length)];
